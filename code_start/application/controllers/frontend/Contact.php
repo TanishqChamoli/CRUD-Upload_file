ef401dd6b291e7455cfe3cee5afd859fc083d0ca
Tanishq Chamoli <tanishq.chamoli@gmail.com>
2020-10-25 18:18:57 +0530

    Task 3

---
+++ code_start/application/controllers/frontend/Contact.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Contact extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('frontend/contact');
+    }
+}
